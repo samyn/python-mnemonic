d401bc824df2cc5e031b54a818bf71fde9a110da
samyn <sam@brokersfriend.com>
2024-10-17 23:40:39 +0800

    random optimize

--- generate_seeds.py
+++ generate_seeds.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 from binascii import hexlify, unhexlify
+import binascii
 from mnemonic import Mnemonic
 import random
 import sys
 import hashlib
+from termcolor import colored 
 
 seeds_peers =  {"12": 128, '18':192, '24':256}
 length = "12"
@@ -16,8 +18,9 @@ if len(sys.argv) > 1:
 key_bits_length = seeds_peers[length]
 code_length = int(key_bits_length / 4)
 mnemo = Mnemonic("english")
-"""
+
 key_hexstr =""
+'''
 print("\n Random HEX:")
 for _ in range(code_length):
     c1 = random.sample("ABCDEF0123456789",1)
@@ -25,10 +28,14 @@ for _ in range(code_length):
     key_hexstr += c1[0]
 #key_hexstr = "912BF6D0C3FE1111E6BC79E76D45FBD4"
 print(key_hexstr)
-"""
+'''
 print("\n PLEASE INPUT PAIR OF DICE STEP BY STEP:")
 key_dice = ""
+key_binary = ""
 key_base6 = ""
+key_4digit6 = ""
+key_4digit10 = ""
+key_4digit10mod = ""
 key_hexstr =""
 key_hexstr_space =""
 dices = []
@@ -36,13 +43,13 @@ p = 1
 pair_no = 0
 while p<=code_length:
     pair_no += 1
-    """
     dice1 = random.sample("123456",1)
     dice2 = random.sample("123456",1)
-    """
+    '''
     print ("\nTRYING {0}/{1}... ".format(p,code_length))
     dice1 = input(" DICE1(1-6):")
     dice2 = input(" DICE2(1-6):")
+    '''
     if len(dice1)==0 or len(dice2)==0:
         print("PLEASE INPUT AGAIN.")
         continue 
@@ -54,33 +61,60 @@ while p<=code_length:
     s2 =int(dice2[0])-1
     n10=s1*6+s2
     if n10 < 32:
-        key_dice += dice1[0]
-        key_dice += dice2[0]
+        key_dice += "  "+ dice1[0] + dice2[0]
+        key_4digit6 += f"  {s1}{s2}"
+        key_binary += "{0:04b}".format(n10%16)
+        key_4digit10 += "{0:4d}".format(n10)
+        key_4digit10mod += "{0:4d}".format(n10%16)
         key_base6 += chr(ord("0")+s1)
         key_base6 += chr(ord("0")+s2)
         c16 = "{0:X}".format(n10 % 16)
         key_hexstr += c16
-        key_hexstr_space += " "+c16
+        key_hexstr_space += "{0:4X}".format(n10 % 16)
         dices.append(dice1[0])
         dices.append(dice2[0])
         #print("Dice pair[",dice1[0],dice2[0],"] base6[",s1,s2,"] base10[",n10,"] HEX[",c16,"]")
-        print("PAIR[{0},{1}] B06[{2},{3}] B10[{4:2d}] B16[{5}]".format(dice1[0],dice2[0],s1,s2,n10,c16))
-        print(dices)
+        print("DICE[{0},{1}] B06[{2},{3}] B10[{4:2d}] B16[{5}]".format(dice1[0],dice2[0],s1,s2,n10,c16))
+        #print(dices)
         p += 1
     else:
-        print("PAIR[{0},{1}] B06[{2},{3}] B10[{4:2d}] IN 32-35, ABANDONED.".format(dice1[0],dice2[0],s1,s2,n10))
-        print(dices)
-
-
+        print("DICE[{0},{1}] B06[{2},{3}] B10[{4:2d}] IN 32-35, ABANDONED.".format(dice1[0],dice2[0],s1,s2,n10))
+        #print(dices)
 print("\nROLLED DICES:",pair_no*2,"(",code_length*2,")")
-print("DCS: ",key_dice)
-print("SIX: ",key_base6)
-print("HEX: ",key_hexstr_space)
-print("HEX: ",key_hexstr)
 
-#key_hexstr = "912BF6D0C3FE1111E6BC79E76D45FBD4"
-print("\n SEEDS[12*/18/24]:"+length)
-print("\n  "+mnemo.to_mnemonic(unhexlify(key_hexstr)))
-print("\n KEY_BITS[128/192/256]:"+str(key_bits_length))
-print("\n  "+key_hexstr)
+
+
+
+# key_hexstr = "912BF6D0C3FE1111E6BC79E76D45FBD4"
+data = unhexlify(key_hexstr)
+mnemonic = mnemo.to_mnemonic(data)
+print("SEEDS[12*/18/24]:"+length)
+print("KEY_BITS[128/192/256]:"+str(key_bits_length))
+print("\n  "+mnemonic+"\n")
+
+
+print("HEX: ",key_hexstr)
+print("DCS: ",key_dice)
+print("SIX: ",key_4digit6)
+print("DEC: ",key_4digit10)
+print("MOD: ",key_4digit10mod)
+print("HEX: ",key_hexstr_space)
+print("BIN: ",key_binary)
+h = hashlib.sha256(data).hexdigest()
+hb = bin(int(h, 16))[2:].zfill(256)[: len(data) * 8 // 32]
+b = bin(int(binascii.hexlify(data), 16))[2:].zfill(len(data) * 8)
+#print(h,  b, hb)
+arr_mnemo = mnemonic.split(" ")
+for i in range(len(arr_mnemo)):
+    colorb = ""
+    if i == len(arr_mnemo)-1:
+        bidx = b[i * 11 :] + hb
+        colorb = colored(b[i * 11 :], 'white') + colored(hb, 'red')
+    else:
+        bidx = b[i * 11 : (i + 1) * 11]
+        colorb = colored(bidx, 'white')
+    idx = int(bidx, 2)
+    print(f"{i:2} {colorb} {idx:4} {arr_mnemo[i]:20}")
+
+
 
